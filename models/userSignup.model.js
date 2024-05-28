1e9a188da5726991e51bc89c5c13ad870348d7a5
sachinchauhan010 <010sssachin@gmail.com>
2024-05-29 03:06:21 +0530

    cart item id store in db

--- models/userSignup.model.js
+++ models/userSignup.model.js
@@ -19,6 +19,10 @@ const userSchema=new mongoose.Schema({
         type:String,
         required:true,
     },
+    cart:{
+        type:Array,
+        default:[],
+    }
 },{timestamps:true});
 
 const User=mongoose.model('User', userSchema);
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,6 @@
 import {Router} from 'express';
 import express from 'express';
-import { authenticateJWT, registerUser, userLogin, userLogout} from '../controllers/user.controller.js';
+import { authenticateJWT, registerUser, userLogin, userLogout, addToCart, getUserCart} from '../controllers/user.controller.js';
 
 
 const router=Router();
@@ -10,5 +10,7 @@ router.route('/register').post(registerUser);
 router.route('/auth').post(authenticateJWT);
 router.route('/login').post(userLogin);
 router.route('/logout').post(userLogout);
+router.route('/add-to-cart').post(addToCart);
+router.route('/get-user-cart').post(getUserCart);
 
 export default router;
