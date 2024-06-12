365149c2f1d7ca34579564bab25755ed6eef830b
sachinchauhan010 <010sssachin@gmail.com>
2024-06-12 22:06:21 +0530

    get request

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 connectDB();
-
+app.get('/',(req, res)=>{
+    res.send('Hello World');
+})
 app.use('/api/user',userrouter);
 app.use ('/api/distributor', DistributorRouter);
 const port=process.env.PORT|| 3000;
